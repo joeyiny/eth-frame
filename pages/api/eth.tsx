6f017c46e6a706f2c18d960b9e5095d082a632fc
joey iny <joeyainy@gmail.com>
2024-01-27 16:21:48 -0800

    Add the date to OG image

--- pages/api/eth.tsx
+++ pages/api/eth.tsx
@@ -9,12 +9,29 @@ import * as fs from "fs";
 const fontPath = join(process.cwd(), "Roboto-Regular.ttf");
 let fontData = fs.readFileSync(fontPath);
 
+function getCurrentDateTime() {
+  const now = new Date();
+
+  // Build the formatted date string manually
+  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const year = now.getFullYear();
+  const month = monthNames[now.getMonth()];
+  const date = now.getDate().toString().padStart(2, "0");
+  const hours = now.getHours() % 12 || 12; // Convert to 12-hour format and adjust 0 to 12
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+  const ampm = now.getHours() >= 12 ? "PM" : "AM";
+
+  return `${month} ${date} ${year} ${hours}:${minutes}:${seconds} ${ampm}`;
+}
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const response = await fetch("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD");
     const data = await response.json();
     const p = data.USD;
     // if (!data) return res.status(400).send("couldnt get eth price");
+    const currentDateTime = getCurrentDateTime();
     const svg = await satori(
       <div
         style={{
@@ -38,6 +55,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             width: "100%",
             padding: 20,
           }}>
+          <span style={{ color: "darkgray", fontSize: "30" }}>{currentDateTime}</span>
           <h2 style={{ textAlign: "center", color: "black", fontSize: "40" }}>$ETH Price</h2>
           <div
             style={{
